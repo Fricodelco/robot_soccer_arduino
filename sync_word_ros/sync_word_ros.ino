d5d3afe18d55b08c3a901971e0e8bcdf32a5560c
Rodion <rodion_anisimov@mail.ru>
2019-03-28 04:43:26 +0300

    add state publisher

--- sync_word_ros/sync_word_ros.ino
+++ sync_word_ros/sync_word_ros.ino
@@ -2,6 +2,7 @@
 //#include "move.h"
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Byte.h>
 #include  <DynamixelWorkbench.h>
 
 #define DEVICE_NAME "3" 
@@ -16,40 +17,39 @@ const uint8_t handler_index = 0;
 
 DynamixelWorkbench dxl_wb;
 #define ROS_RATE 50000
-ros::NodeHandle_<ArduinoHardware, 2, 2, 150, 150> nh;
+ros::NodeHandle_<ArduinoHardware, 3, 3, 150, 150> nh;
 //ros::NodeHandle nh;
 int16_t y = 0;
+int16_t x = 0;
 int16_t Motion_number = 0;
 bool Ball_Detect = false;
 //HardwareTimer Timer(TIMER_CH4);
 
-std_msgs::Int16 state_msg;
-ros::Publisher pub_state("dxl_state", &state_msg);
+std_msgs::Byte state_msg;
+ros::Publisher pub_state("motion_ready", &state_msg);
 
 bool start_flag = true;
 void neckY(const std_msgs::Int16& errY)
 {
-   if(errY.data < 555)//555 if ball not detect
-   { 
-    Ball_Detect = true; 
-    if (abs(errY.data)>30)
-    {
-      y = y + 0.03*errY.data;
-      if(y>60)
-        y=60;
-      else if(y<-60)
-        y=-60; 
-       
-    }
-   }
-   else
+  if(errY.data < 300){
+    y = errY.data;
+    Ball_Detect = true;}
+  else
     Ball_Detect = false;
-    
+}
+void neckX(const std_msgs::Int16& errX)
+{
+  if(errX.data < 300){
+    x = errX.data;
+    Ball_Detect = true;}
+  else
+    Ball_Detect = false;
 }
 void motion(const std_msgs::Int16& motion){
   Motion_number = motion.data;
 }
-ros::Subscriber<std_msgs::Int16> neck_sub("forward_y_err", neckY );
+ros::Subscriber<std_msgs::Int16> neck_subY("forward_y_err", neckY );
+ros::Subscriber<std_msgs::Int16> neck_subX("forward_x_err", neckX );
 ros::Subscriber<std_msgs::Int16> motion_sub("forward_action", motion );
 
 HardwareTimer Timer(TIMER_CH4);
@@ -58,7 +58,8 @@ void setup()
   pinMode(13, INPUT);
   model_number = 0;
   dxl_wb.init(DEVICE_NAME, BAUDRATE);
-  for (int i = 0; i<20; i++){
+  for (int i = 0;
+  i<20; i++){
     dxl_id[i]=i+1;
   }
   for (int cnt = 0; cnt < 6; cnt++)
@@ -81,7 +82,8 @@ void setup()
   // while(!Serial); // Wait for Opening Serial Monitor
   nh.getHardware()->setBaud(1000000); 
   nh.initNode(); 
-  nh.subscribe(neck_sub);
+  nh.subscribe(neck_subY);
+  nh.subscribe(neck_subX);
   nh.subscribe(motion_sub);
   nh.advertise(pub_state);
   //ready_for_walk(0,y);
@@ -97,58 +99,72 @@ void setup()
 void loop() 
 {  
  if(digitalRead(13) == 0){
-   
+  state_msg.data = true;
   switch (Motion_number){
   case 0:
-    ready_for_walk(0,y);
+    ready_for_walk(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 1:
-    FFT_M(0,y);
+    FFT_M(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 2:
-    FLT_M(0,y);
+    FLT_M(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 3:
-    FRT_M(0,y);
+    FRT_M(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 4:
-    TURN_L(0,y);
+    TURN_L(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 5:
-    TURN_R(0,y);
+    TURN_R(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 6:
-    F_Shoot_L(0,y);
+    F_Shoot_L(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 7:
-    F_Shoot_R(0,y);
+    F_Shoot_R(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 8:
-    M_L(0,y);
+    M_L(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 9:
-    M_R(0,y);
+    M_R(x,y);
+    pub_state.publish(&state_msg);
     break;
   case 10:
-    //ready_for_walk(0,y);
-    Search_Ball();
+    ready_for_walk(x,y);
+    pub_state.publish(&state_msg);
+    //Search_Ball();
     break;
   case 11:
-    GET_UP_FRONT(0,y);
+    GET_UP_FRONT(0,-20);
+    pub_state.publish(&state_msg);
     break;
   case 12:
-    GET_UP_BACK(0,y);
+    GET_UP_BACK(0,-20);
+    pub_state.publish(&state_msg);
     break;
   default:
-    ready_for_walk(0,y);
+    ready_for_walk(x,y);
+    pub_state.publish(&state_msg);
     break;
   }
  }
  else{
-  ready_for_walk(0,y);
- }
-  state_msg.data = y;
+  
+  ready_for_walk(x,y);
   pub_state.publish(&state_msg);
+ }
   //nh.spinOnce();
   
 }
@@ -230,13 +246,13 @@ void M_L(int w1, int w2){
   setMotors(-90,90,-68,68,-14,14,-45,45,-1,1,-70,70,-103,103,51,-51,-1,1,w2,w1,100);
 }
 void  TURN_R(int w1, int w2){
-  setMotors(-90,90,-68,68,-14,14,-26,21,-1,1,-78,69,-117,103, 60,-51, -1,-1,w2,w1,70);
-  setMotors(-90,90,-68,68,-14,14,-30,30,-1,1,-69,69,-103,103, 51,-51, -1,-1,w2,w1,100);
+  setMotors(-90,90,-68,68,-14,14,-31,26,-1,1,-78,69,-117,103, 60,-51, -1,-1,w2,w1,70);
+  setMotors(-90,90,-68,68,-14,14,-35,35,-1,1,-69,69,-103,103, 51,-51, -1,-1,w2,w1,100);
   setMotors(-90,90,-68,68,-14,14,-45,45,-1,1,-72,72,-103,102,51,-51,-1,1,w2,w1,200);
 }
 void TURN_L(int w1, int w2){
-  setMotors(-90,90,-68,68,-14,14,-21,26,-1,1,-69,78,-103,117, 51,-61, -1,-1,w2,w1,70);
-  setMotors(-90,90,-68,68,-14,14,-30,30,-1,1,-69,69,-103,103, 51,-51, -1,-1,w2,w1,100);
+  setMotors(-90,90,-68,68,-14,14,-26,31,-1,1,-69,78,-103,117, 51,-61, -1,-1,w2,w1,70);
+  setMotors(-90,90,-68,68,-14,14,-35,35,-1,1,-69,69,-103,103, 51,-51, -1,-1,w2,w1,100);
   setMotors(-90,90,-68,68,-14,14,-45,45,-1,1,-72,72,-103,102,51,-51,-1,1,w2,w1,200);
 }
 void F_Shoot_R(int w1, int w2){
@@ -268,45 +284,61 @@ void GET_UP_FRONT(int w1, int w2){
 }
 void Search_Ball(){
   while((Ball_Detect == false) && (Motion_number == 10)){
-    ready_for_walk(0,-20); // head up
+    ready_for_walk(0,0); // head up
     wait(200);
-      if(Ball_Detect == true){y=-20;break;}
+      if(Ball_Detect == true){y=0;break;}
 
 
-    ready_for_walk(0,-40); // head down
+    ready_for_walk(0,-30); // head down
     wait(200);
-      if(Ball_Detect == true){y=-40;break;}
+      if(Ball_Detect == true){y=-30;break;}
 
           
-    ready_for_walk(0,-55); //head down
-      if(Ball_Detect == true){y=-55;break;}
+    ready_for_walk(0,-60); //head down
+      if(Ball_Detect == true){y=-60;break;}
     wait(200);
-
-    
-    ready_for_walk(30,-20); //head left 
+    if (x>0){
+      if(HeadLeft() == true){break;}
+      if(HeadRight() == true){break;}
+      TURN_L(0,y);
+      TURN_L(0,y);
+      TURN_L(0,y);
+      TURN_L(0,y); 
+    }
+    else{   
+      if(HeadRight() == true){break;}
+      if(HeadLeft() == true){break;}
+      TURN_R(0,y);
+      TURN_R(0,y);
+      TURN_R(0,y);
+      TURN_R(0,y); 
+    }  
+  }
+}
+bool HeadLeft(){
+    ready_for_walk(30,0); //head left 
     wait(300);
-      if(Ball_Detect == true){y=-20;TURN_L(0,y);break;}
-    ready_for_walk(30,-40); // 
+      if(Ball_Detect == true){x=30,y=0;return true;}
+    ready_for_walk(30,-30); // 
     wait(200);
-      if(Ball_Detect == true){y=-40;TURN_L(0,y);break;} 
+      if(Ball_Detect == true){x=30,y=-30;return true;} 
     ready_for_walk(30,-60); // 
     wait(200);
-      if(Ball_Detect == true){y=-60;TURN_L(0,y);break;} 
+      if(Ball_Detect == true){x=30,y=-60;return true;}
+    return false; 
+}
 
-       
+bool HeadRight(){
     ready_for_walk(-30,-20); //head right 
     wait(300);
-      if(Ball_Detect == true){y=-20;TURN_R(0,y);break;}
+      if(Ball_Detect == true){x=-30,y=-20;return true;}
     ready_for_walk(-30,-40); // 
     wait(200);
-      if(Ball_Detect == true){y=-40;TURN_R(0,y);break;} 
+      if(Ball_Detect == true){x=-30,y=-40;return true;} 
     ready_for_walk(-30,-55); // 
     wait(200);
-      if(Ball_Detect == true){y=-55;TURN_R(0,y);break;} 
-    TURN_L(0,y);
-    TURN_L(0,y);
-    TURN_L(0,y); 
-  }
+      if(Ball_Detect == true){x=-30,y=-55;return true;}
+    return false;
 }
 void setMotorsEnc(int m1,int m2,int m3,int m4,int m5,int m6,int m7,int m8,int m9,int m10,int m11,int m12,int m13,int m14,int m15,int m16,int m17,int m18,int m19,int m20,int delayy){
   bool result = false;
